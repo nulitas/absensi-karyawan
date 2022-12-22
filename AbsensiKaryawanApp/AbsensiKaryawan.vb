77626e443e50a8f8d4647e2c7c8f211edfd56e73
nulitas <ndrakyta@gmail.com>
2022-12-22 21:21:55 +0700

    Menambahkan form jabatan

--- AbsensiKaryawanApp/AbsensiKaryawan.vb
+++ AbsensiKaryawanApp/AbsensiKaryawan.vb
@@ -2,4 +2,7 @@
 
     Public Shared GSAbsensi = New GSAbsensi
 
+    Private Sub BtnFormJabatan_Click(sender As Object, e As EventArgs) Handles BtnFormJabatan.Click
+        Jabatan.Show()
+    End Sub
 End Class
---
+++ AbsensiKaryawanApp/Jabatan.vb
@@ -0,0 +1,3 @@
+﻿Public Class Jabatan
+
+End Class
