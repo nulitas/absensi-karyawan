da675e990eaee308ac444d3ad938b70400a2bedb
zachisoni <muhammad.asaduddin.1.19.1.4@gmail.com>
2022-12-23 18:44:04 +0700

    mengubah nama form

--- AbsensiKaryawanApp/Absensi.Designer.vb
+++ AbsensiKaryawanApp/Absensi.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class FormAbsensi
+Partial Class Absensi
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,16 @@ Partial Class FormAbsensi
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'Absensi
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "FormAbsensi"
+        Me.ClientSize = New System.Drawing.Size(547, 364)
+        Me.Name = "Absensi"
+        Me.Text = "Absensi"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
---
+++ AbsensiKaryawanApp/Absensi.vb
@@ -0,0 +1,3 @@
+﻿Public Class Absensi
+
+End Class
