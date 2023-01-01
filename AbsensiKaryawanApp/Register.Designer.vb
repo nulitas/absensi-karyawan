7a4e0daea856c5088e2a3f0307af6702e46f2ff0
nulitas <ndrakyta@gmail.com>
2023-01-01 13:15:46 +0700

    Mengubah posisi input di Register

--- AbsensiKaryawanApp/Register.Designer.vb
+++ AbsensiKaryawanApp/Register.Designer.vb
@@ -52,20 +52,18 @@ Partial Class Register
         '
         Me.TxtUsername.BackColor = System.Drawing.SystemColors.ButtonFace
         Me.TxtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.TxtUsername.Location = New System.Drawing.Point(6, 0)
-        Me.TxtUsername.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TxtUsername.Location = New System.Drawing.Point(6, 8)
         Me.TxtUsername.Name = "TxtUsername"
-        Me.TxtUsername.Size = New System.Drawing.Size(190, 16)
+        Me.TxtUsername.Size = New System.Drawing.Size(253, 20)
         Me.TxtUsername.TabIndex = 1
         '
         'TxtPassword
         '
         Me.TxtPassword.BackColor = System.Drawing.SystemColors.ButtonFace
         Me.TxtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.TxtPassword.Location = New System.Drawing.Point(5, 6)
-        Me.TxtPassword.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TxtPassword.Location = New System.Drawing.Point(6, 8)
         Me.TxtPassword.Name = "TxtPassword"
-        Me.TxtPassword.Size = New System.Drawing.Size(190, 16)
+        Me.TxtPassword.Size = New System.Drawing.Size(253, 20)
         Me.TxtPassword.TabIndex = 2
         '
         'BtnRegister
@@ -73,10 +71,9 @@ Partial Class Register
         Me.BtnRegister.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.BtnRegister.Font = New System.Drawing.Font("Segoe UI", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.BtnRegister.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.BtnRegister.Location = New System.Drawing.Point(215, 238)
-        Me.BtnRegister.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.BtnRegister.Location = New System.Drawing.Point(246, 317)
         Me.BtnRegister.Name = "BtnRegister"
-        Me.BtnRegister.Size = New System.Drawing.Size(231, 34)
+        Me.BtnRegister.Size = New System.Drawing.Size(264, 45)
         Me.BtnRegister.TabIndex = 3
         Me.BtnRegister.Text = "SIGN UP"
         Me.BtnRegister.UseVisualStyleBackColor = False
@@ -89,36 +86,35 @@ Partial Class Register
         '
         Me.TxtConfirmPassword.BackColor = System.Drawing.SystemColors.ButtonFace
         Me.TxtConfirmPassword.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.TxtConfirmPassword.Location = New System.Drawing.Point(7, 7)
-        Me.TxtConfirmPassword.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TxtConfirmPassword.Location = New System.Drawing.Point(7, 8)
         Me.TxtConfirmPassword.Name = "TxtConfirmPassword"
-        Me.TxtConfirmPassword.Size = New System.Drawing.Size(190, 16)
+        Me.TxtConfirmPassword.Size = New System.Drawing.Size(252, 20)
         Me.TxtConfirmPassword.TabIndex = 4
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(98, 70)
+        Me.Label1.Location = New System.Drawing.Point(112, 93)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(60, 15)
+        Me.Label1.Size = New System.Drawing.Size(75, 20)
         Me.Label1.TabIndex = 5
         Me.Label1.Text = "Username"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(98, 136)
+        Me.Label2.Location = New System.Drawing.Point(112, 181)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(57, 15)
+        Me.Label2.Size = New System.Drawing.Size(70, 20)
         Me.Label2.TabIndex = 6
         Me.Label2.Text = "Password"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(368, 138)
+        Me.Label3.Location = New System.Drawing.Point(421, 184)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(117, 15)
+        Me.Label3.Size = New System.Drawing.Size(145, 20)
         Me.Label3.TabIndex = 7
         Me.Label3.Text = "Konfirmasi Password"
         '
@@ -137,18 +133,17 @@ Partial Class Register
         Me.Panel1.Controls.Add(Me.Panel2)
         Me.Panel1.Controls.Add(Me.Panel3)
         Me.Panel1.Controls.Add(Me.Panel4)
-        Me.Panel1.Location = New System.Drawing.Point(10, 62)
-        Me.Panel1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Panel1.Location = New System.Drawing.Point(11, 83)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(658, 351)
+        Me.Panel1.Size = New System.Drawing.Size(752, 467)
         Me.Panel1.TabIndex = 10
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(368, 70)
+        Me.Label7.Location = New System.Drawing.Point(421, 93)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(36, 15)
+        Me.Label7.Size = New System.Drawing.Size(46, 20)
         Me.Label7.TabIndex = 16
         Me.Label7.Text = "Email"
         '
@@ -156,47 +151,45 @@ Partial Class Register
         '
         Me.Panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Panel5.Controls.Add(Me.TxtEmail)
-        Me.Panel5.Location = New System.Drawing.Point(368, 93)
-        Me.Panel5.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Panel5.Location = New System.Drawing.Point(421, 124)
         Me.Panel5.Name = "Panel5"
-        Me.Panel5.Size = New System.Drawing.Size(231, 30)
+        Me.Panel5.Size = New System.Drawing.Size(264, 39)
         Me.Panel5.TabIndex = 14
         '
         'TxtEmail
         '
         Me.TxtEmail.BackColor = System.Drawing.SystemColors.ButtonFace
         Me.TxtEmail.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.TxtEmail.Location = New System.Drawing.Point(6, 1)
-        Me.TxtEmail.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TxtEmail.Location = New System.Drawing.Point(7, 8)
         Me.TxtEmail.Name = "TxtEmail"
-        Me.TxtEmail.Size = New System.Drawing.Size(190, 16)
+        Me.TxtEmail.Size = New System.Drawing.Size(252, 20)
         Me.TxtEmail.TabIndex = 1
         '
         'Label6
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label6.Location = New System.Drawing.Point(293, 22)
+        Me.Label6.Location = New System.Drawing.Point(335, 29)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(84, 24)
+        Me.Label6.Size = New System.Drawing.Size(106, 29)
         Me.Label6.TabIndex = 11
         Me.Label6.Text = "Sign Up"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(237, 279)
+        Me.Label5.Location = New System.Drawing.Point(271, 372)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(123, 15)
+        Me.Label5.Size = New System.Drawing.Size(153, 20)
         Me.Label5.TabIndex = 12
         Me.Label5.Text = "Sudah memiliki akun?"
         '
         'LinkLogin
         '
         Me.LinkLogin.AutoSize = True
-        Me.LinkLogin.Location = New System.Drawing.Point(368, 279)
+        Me.LinkLogin.Location = New System.Drawing.Point(421, 372)
         Me.LinkLogin.Name = "LinkLogin"
-        Me.LinkLogin.Size = New System.Drawing.Size(43, 15)
+        Me.LinkLogin.Size = New System.Drawing.Size(54, 20)
         Me.LinkLogin.TabIndex = 11
         Me.LinkLogin.TabStop = True
         Me.LinkLogin.Text = "Sign In"
@@ -205,50 +198,46 @@ Partial Class Register
         '
         Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Panel2.Controls.Add(Me.TxtUsername)
-        Me.Panel2.Location = New System.Drawing.Point(98, 92)
-        Me.Panel2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Panel2.Location = New System.Drawing.Point(112, 123)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(231, 30)
+        Me.Panel2.Size = New System.Drawing.Size(264, 39)
         Me.Panel2.TabIndex = 13
         '
         'Panel3
         '
         Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Panel3.Controls.Add(Me.TxtPassword)
-        Me.Panel3.Location = New System.Drawing.Point(98, 159)
-        Me.Panel3.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Panel3.Location = New System.Drawing.Point(112, 212)
         Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(231, 30)
+        Me.Panel3.Size = New System.Drawing.Size(264, 39)
         Me.Panel3.TabIndex = 14
         '
         'Panel4
         '
         Me.Panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Panel4.Controls.Add(Me.TxtConfirmPassword)
-        Me.Panel4.Location = New System.Drawing.Point(368, 159)
-        Me.Panel4.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Panel4.Location = New System.Drawing.Point(421, 212)
         Me.Panel4.Name = "Panel4"
-        Me.Panel4.Size = New System.Drawing.Size(231, 30)
+        Me.Panel4.Size = New System.Drawing.Size(264, 39)
         Me.Panel4.TabIndex = 15
         '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.Label4.Location = New System.Drawing.Point(238, 34)
+        Me.Label4.Location = New System.Drawing.Point(246, 43)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(252, 20)
+        Me.Label4.Size = New System.Drawing.Size(309, 25)
         Me.Label4.TabIndex = 16
         Me.Label4.Text = "Daftarkan diri anda sekarang juga!"
         '
         'Register
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(690, 432)
+        Me.ClientSize = New System.Drawing.Size(789, 576)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Panel1)
-        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Register"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Register"
